7a5c5500c45c328e49249fc84acb3231e483770c
NordLex <g.nordlex@gmail.com>
2022-02-10 22:19:16 +0300

    Creating an enumeration of decryption codes

--- app/src/main/java/com/nordlex/paylist/data/DatabaseHelper.java
+++ app/src/main/java/com/nordlex/paylist/data/DatabaseHelper.java
@@ -1,4 +1,35 @@
 package com.nordlex.paylist.data;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation. Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private final Context context;
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Contacts.db";
+    private static final String TABLE_NAME = "Contacts";
+
+    private static final String KEY_ID = "_id";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "create table "+TABLE_NAME+"("+KEY_ID+"";
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
